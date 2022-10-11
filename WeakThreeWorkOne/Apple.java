562ec86a8ace2cb95e75ff44679e0e51252a07f0
fodiloglu-a <109350249+fodiloglu-a@users.noreply.github.com>
2022-10-11 16:01:09 +0300

    JavaBootcamp  HomeWork  Weak_two
    JavaBootcamp  HomeWork  Weak_two

---
+++ WeakThreeWorkOne/Apple.java
@@ -0,0 +1,8 @@
+package WeakThreeWorkOne;
+
+public class Apple extends Fruit{
+    public Apple(String name ,int storeHouse,int price,int Id) {
+        super(name ,storeHouse,price,Id);
+    }
+
+}
